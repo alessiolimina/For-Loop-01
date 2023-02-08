52fb42e24f587130b2e6ec82528711bef4104c5b
Alessio Limina <alessio.limina@gmail.com>
2023-02-08 21:17:48 +0100

    Exercise For Loop 01

--- src/it/develhope/forloop/Start.java
+++ src/it/develhope/forloop/Start.java
@@ -9,10 +9,12 @@ public class Start {
 
         System.out.println("-----------------Starting-------------------");
 
-        List<Integer> numbers = new ArrayList<>();
-        numbers.addAll(Arrays.asList(5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25));
-        for (Integer number: numbers)
-        {System.out.print(number + " ");
+        for(int i = 5; i<=25; i++){
+            System.out.println(i);
+
+        System.out.println("---------------------------------------------");
+
         }
     }
 }
+
