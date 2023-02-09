7a0d5c921c627916cb4247b0e8aadbaaa3ab1f66
Alessio Limina <alessio.limina@gmail.com>
2023-02-09 10:57:16 +0100

    Exercise For Loop 01 Fixed

--- src/it/develhope/forloop/Start.java
+++ src/it/develhope/forloop/Start.java
@@ -1,20 +1,14 @@
 package it.develhope.forloop;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
 public class Start {
     public static void main(String[] args) {
 
         System.out.println("-----------------Starting-------------------");
 
         for(int i = 5; i<=25; i++){
-            System.out.println(i);
+            System.out.println(i);}
 
         System.out.println("---------------------------------------------");
-
-        }
     }
 }
 
